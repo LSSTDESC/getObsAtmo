aea3e3a40a1844109bce7c3a7b76a3fcbab5361b
Sylvie Dagoret-Campagne <sylvie.dagoret-campagne@ijclab.in2p3.fr>
2023-11-01 17:04:53 +0100

    implement first tests

--- tests/test_getObsAtmo.py
+++ tests/test_getObsAtmo.py
@@ -1,26 +1,25 @@
 import unittest
-from getCalspec import is_calspec, Calspec
-from astropy.table import Table as astropyTable
+from getObsAtmo import is_obssite, ObsAtmo
+import numpy as np 
 
 
-class GetCalspecTestCase(unittest.TestCase):
+
+class GetObsAtmoTestCase(unittest.TestCase):
     """A test case for the getCalspec package."""
 
-    def test_is_calspec(self):
-        self.assertTrue(is_calspec("eta1 dor"))
-        self.assertFalse(is_calspec("NotACalspecStar"))
-        self.assertFalse(is_calspec("Not A Calspec Star With Spaces"))
+    def test_is_obssite(self):
+        self.assertTrue(is_obssite("LSST"))
+        self.assertTrue(is_obssite("CTIO"))
+        self.assertTrue(is_obssite("OMK"))
+        self.assertFalse(is_obssite("AMissingObs"))
+        self.assertTrue(is_obssite("L s s t"))
 
-    def test_Calspec(self):
-        c = Calspec('eta dor')
-        table = c.get_spectrum_table()
-        self.assertIsInstance(table, astropyTable)
+    def test_ObsAtmo(self):
+        e = ObsAtmo('LSST')
+        array = e.GetAllTransparencies([300.,400.,600.,800.,1000.],1.,0.,0.)
+        self.assertTrue(np.allclose(array,[0.41483073, 0.76797745, 0.95170851, 0.98476396, 0.9937721 ]),msg="ObsAtmo.GetAllTransparencies error")
 
-        data = c.get_spectrum_numpy()
-        self.assertIsInstance(data, dict)
-        expectedKeys = ['WAVELENGTH', 'FLUX', 'STATERROR', 'SYSERROR']
-        for key in expectedKeys:
-            self.assertIn(key, data.keys())
+        
 
 
 if __name__ == "__main__":
